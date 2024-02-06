7b41d683c35577c799fce013ba617a570368be83
Nimo Shirille <shirille21@gmail.com>
2024-02-05 18:58:13 -0600

    .

--- simple.js
+++ simple.js
@@ -1,80 +1,85 @@
-var quiz = [
-    {
-        question : "What does CSS stand for?",
-        options : ['Cascading Script Style','Cascading Style Sheets', 'Computer Style Sheets','Creative Styling Solutions',],
-        answer : 'Cascading Style Sheets'
-    },
-    {
-        question : "How is CSS typically used in web development?",
-        options : ['To define the structure and content of a web page','To add interactivity and functionality to a web page', 'To describe the presentation and style of a web page','To manage server-side operations of a web page',],
-        answer : 'To describe the presentation and style of a web page'
-    },
-    {
-        question : "Which HTML attribute is used to apply CSS styles directly to an HTML element?",
-        options : ['style','css', 'class','id',],
-        answer : 'style'
-    },
-    {
-        question : "Which CSS property is used to control the size and layout of boxes in CSS?",
-        options : ['color','font-family', 'width','background-color',],
-        answer : 'width'
-    },
-    {
-        question : "What is the correct CSS syntax to select an element with a specific class?",
-        options : ['.class','#class', '.class-name','class:',],
-        answer : '.class'
+var currentWeatherDiv = document.querySelector('#weather-cards');
+var weatherCardDiv = document.querySelector('.weather-cards');
+var cityInput = document.querySelector('#city-input');
+var searchButton = document.querySelector('#submit');
+var API_KEY = "c40ec961139c146223010acccae3fa44";
+
+var createWeatherCard = (cityName, weatherItem, index) => {
+    if (index === 0) {
+        return `
+        <div class="weather-container">
+            <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+            <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+            <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+            <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+        </div>
+        <div>
+            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather Icon">
+        </div>`;
+    } else {
+        return `
+        <li class="cards">
+            <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather Icon">
+            <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+            <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+            <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+        </li>`;
     }
+    };
 
-];
-var question = document.getElementById('question');
-var answerbutton = document.getElementById('options');
-var nextbutton = document.getElementById('next-btn');
-
-
-var currentQuestionIndex = 0;
-var score = 0;
-var currentQuestion= quiz[currentQuestionIndex];
-
-// Function to display the current question
-        function displayQuestion() {
-        var currentQuestion= quiz[currentQuestionIndex];
-        var questionnum = currentQuestionIndex + 1;
-
-        questionElement.innerHTML = questionnum + quiz[currentQuestion].question;
-
-        optionsElement.innerHTML = "";
-
-        quiz[currentQuestion].options.forEach(option => {
-            var button = document.createElement("button");
-            button.textContent = option;
-            button.addEventListener("click", handleAnswer);
-            optionsElement.appendChild(button);
+    var getWeatherDetails = (cityName, lat, lon) => {
+    var WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat${lat}&lon${lo}&appid=${API_KEY}`;
+    console.log(data)
+    fetch(WEATHER_API_URL)
+        .then(res => res.json())
+        .then(data => {
+        // filter the forecast to get only one per day
+        var uniqueForecastDays = [];
+        var fiveDaysForecast = data.list.filter(forecast => {
+            var forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+            return uniqueForecastDays.push(forecastDate);
+            }
         });
-        }
 
-        // Function to handle user's answer
-        function handleAnswer(event) {
-        var selectedOption = event.target.textContent;
-        var correctAnswer = quiz[currentQuestion].answer;
+        // Clear previous weather cards
+        weatherCardDiv.value.innerHTML= "";
+        cityInput.value = "";
 
-        if (selectedOption === correctAnswer) {
-            score++;
-        }
+        console.log(fiveDaysForecast);
 
-        currentQuestion++;
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            weatherCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+        });
 
-        if (currentQuestion < quiz.length) {
-            displayQuestion();
-        } else {
-            displayScore();
-        }
-        }
+        // Store the weather data in localStorage
+        localStorage.setItem('weatherData', JSON.stringify(data));
+        })
+        .catch(() => {
+        alert("An error occurred while fetching the weather forecast!");
+        });
+    };
 
-        // Function to display the final score
-        function displayScore() {
-        var scoreElement = document.getElementById("score");
-        scoreElement.textContent = `Your score: ${score}/${quiz.length}`;
-    }
+    var getCityCoordinates = () => {
+    var cityName = cityInput.value;
+    if (!cityName) return;
 
-    // Display the first question
-    displayQuestion();
+    var GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${API_KEY}`;
+    console.log()
+    
+    fetch(GEOCODING_API_URL)
+        .then(res => res.json())
+        .then(data => {
+        if (data.length === 0) return alert(`No coordinates found for ${cityName}`);
+
+        var { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
+        })
+        .catch(() => {
+        alert("Error fetching weather data!");
+        });
+
+};
+
+searchButton.addEventListener('click', getCityCoordinates);
